c28f8b98fabae82289a64ee09d3293b57b1ca7e3
Pavel <avelnet@outlook.com>
2025-11-14 18:39:32 +0700

    Доделать кнопку(скрипт)

---
+++ scripts/scripts.js
@@ -0,0 +1,40 @@
+const links = document.querySelectorAll(".banner-btn");
+
+for (const link of links) {
+link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
+}
+
+
+window.onscroll = function(){scrollFunction()};
+
+const upbuttons = document.querySelectorAll(".button-up");
+
+for (const upbutton of upbuttons) {
+    upbutton.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+});
+}
+
+function scrollFunction(){
+    if (document.body.scrollTop > 1000 || document.documentElementscrollTop > 1000){
+    document.getElementById('btnUp').className = 'button-up visible';
+    } else {
+        document.getElementById('btnUp').className = 'button-up hidden';
+    }
+}
